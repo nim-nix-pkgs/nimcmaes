ffd9f2acf20105ff85cb1d8a411d5eea749986f3
hugosenari <hugosenari@gmail.com>
2022-03-12 04:26:16 -0300

    chore: re index nimcmaes

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim CMAES library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimcmaes-master.flake = false;
+  inputs.src-nimcmaes-master.owner = "zevv";
+  inputs.src-nimcmaes-master.ref   = "refs/heads/master";
+  inputs.src-nimcmaes-master.repo  = "nimcmaes";
+  inputs.src-nimcmaes-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimcmaes-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimcmaes-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
